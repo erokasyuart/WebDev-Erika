e1d366cbcb95a9f23d709c41ee384a80348380b9
erokasyuart <erikat.stuart@gmail.com>
2022-10-31 16:46:04 +1300

    Added API fetching in js

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const BASE_URL = `https://api.unsplash.com`;
+let images = fetch (`${BASE_URL}/search/photos/?query="skincare&client id=56nlzPl65-YaTzlU_1Z-57ZvobZwvOAgPdfPUkCHO7U`);
+images.then{response =>{
+    //stuff
+}}
