f6dfe8ad0274fc93c6f6715b19349f7ed16b1bf1
erokasyuart <erikat.stuart@gmail.com>
2022-11-07 16:25:19 +1300

    Attempt at scroll-into-view-animation text, added a code pen link and its function commented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,30 @@
-const BASE_URL = `https://api.unsplash.com`;
-let images = fetch (`${BASE_URL}/search/photos/?query="skincare&client id=56nlzPl65-YaTzlU_1Z-57ZvobZwvOAgPdfPUkCHO7U`);
-images.then{response =>{
-    //stuff
-}}
+const BASE_URL = "https://api.unsplash.com";
+let images = fetch (`${BASE_URL}/search/photos/?query="skincare&client_id=56nlzPl65-YaTzlU_1Z-57ZvobZwvOAgPdfPUkCHO7U`);
+images.then(response =>{
+    return response.json();
+}).then (data =>{
+    console.log(data);
+})
 
-const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-      // If the element is visible
-      if (entry.isIntersecting) {
-        // Add the animation class
-        entry.target.classList.add('slide-up');
-      }
-    });
-  });
-  
-  observer.observe(document.querySelector('.slide-up'));
+/*https://codepen.io/jr-cologne/pen/zdYdmx
+let text = document.getElementsByClassName('slide-up')
+let elementHeight = text.clientHeight;
+document.addEventListener('scroll', animate);
+
+function inView(){
+  let height = window.innerHeight; //window height
+  let scrollY = window.scrollY || window.pageYOffset; //counts pixels that have been scrolled
+  let scrollPos = scrollY + height;
+  let elementPos = text.getBoundingClientRect().top + scrollY + elementHeight;
+  if (scrollPosition > elementPosition) {
+    return true;
+  }
+  return false;
+}
+
+function animate(){
+  if (inView()){
+    text.classList.add('slide-up');
+  }
+}
+*/
