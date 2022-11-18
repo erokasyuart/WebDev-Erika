c40f79a16622475771210658c67de06177186366
Erika Stuart <erikat.stuart@gmail.com>
2022-11-19 05:06:08 +1300

    Skincare images now appear hidden

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,13 +12,6 @@ let images = [
   document.querySelector(".img10")
 ];
 
-let all = [];
-let skincare =[];
-let honey = [];
-let packs = [];
-let kawakawa = [];
-let manuka = [];
-let harakeke = [];
 
 //API
 const BASE_URL = "https://api.unsplash.com"; //Unsplash website
@@ -33,58 +26,61 @@ fetch (`${BASE_URL}/search/photos/?query="skincare&client_id=56nlzPl65-YaTzlU_1Z
 //checking if the item is in view to trigger animation
 let isInViewport = (item) => {
   let bounding = item.getBoundingClientRect(),
-      myElementHeight = item.offsetHeight;
-
+  myElementHeight = item.offsetHeight;
+  
   if(bounding.top >= -myElementHeight
-      && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
+    && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
       return true;
-  } else {
+    } else {
       return false;
+    }
   }
-}
-
-//for text that slides up animation
-const movingText = document.querySelectorAll(".up");
-console.log('movingText', movingText);
-
-for(let i=0; i<movingText.length;i++){
-  window.addEventListener("scroll", function(){
+  
+  //for text that slides up animation
+  const movingText = document.querySelectorAll(".up");
+  console.log('movingText', movingText);
+  
+  for(let i=0; i<movingText.length;i++){
+    window.addEventListener("scroll", function(){
       if(isInViewport(movingText[i])) {
         movingText[i].classList.add("slide-up"); 
-    }
-  })
-};
-
-//For shipping animation from left to right
-const movingRight = document.querySelectorAll(".right-animation");
-console.log('movingRight', movingRight);
-
-for(let i=0; i<movingRight.length;i++){
-  window.addEventListener("scroll", function(){
+      }
+    })
+  };
+  
+  //For shipping animation from left to right
+  const movingRight = document.querySelectorAll(".right-animation");
+  console.log('movingRight', movingRight);
+  
+  for(let i=0; i<movingRight.length;i++){
+    window.addEventListener("scroll", function(){
       if(isInViewport(movingRight[i])) {
         movingRight[i].classList.add("slide-right"); 
-    }
-  })
-};
-
-for (let i=0; i<images.length; i++){
-  all.push(document.querySelectorAll(".all"))
-  honey.push(document.querySelectorAll(".honey"))
-  skincare.push(document.querySelectorAll(".skincare"))
-  packs.push(document.querySelectorAll(".packs"))
-  manuka.push(document.querySelectorAll(".manuka"))
-  harakeke.push(document.querySelectorAll(".harakeke"))
-}
+      }
+    })
+  };
+  
+  //all the images that have that class
+  let all = document.querySelectorAll(".all");
+  let skincare = document.querySelectorAll(".skincare");
+  let honey = document.querySelectorAll(".honey");
+  let packs = document.querySelectorAll(".packs");
+  let kawakawa = document.querySelectorAll(".kawakawa");
+  let manuka = document.querySelectorAll(".manuka");
+  let harakeke = document.querySelectorAll(".harakeke");
 
 const radioButtons = document.querySelectorAll('input');
 for (const radioButton of radioButtons){
   radioButton.addEventListener('change',function(e){
+
     if (document.getElementById('skincare').checked == true){
       console.log("skincare button");
-      
-      }
-  });
+      for (let i = 0; i < skincare.length; i++){
+        skincare[i].style.visibility = "hidden";
 
+      }
+  }})
 }
+
 
             
